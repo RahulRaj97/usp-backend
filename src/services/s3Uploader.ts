99922c29628bec375f202194608559f9deb888a7
Rahul Raj <rahule_lohana@hotmail.com>
2025-06-08 15:11:02 +0200

    [feat] - fix issue

--- src/services/s3Uploader.ts
+++ src/services/s3Uploader.ts
@@ -7,8 +7,8 @@ import path from 'path';
 export const s3 = new S3Client({
   region: process.env.AWS_REGION || 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
